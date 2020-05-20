7f94fcb8d109ab1d77ca41db6a66045d5a62c159
Hadrian Hughes <hadrian.hughes@and.digital>
2020-05-20 12:02:17 +0100

    Create simple styled Graph component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Graph from './components/Graph';
+import { RecoilRoot } from 'recoil';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <RecoilRoot>
+      <Graph />
+    </RecoilRoot>
+  </div>
+);
 
 export default App;
---
+++ src/components/Graph/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const WIDTH = 1000;
+const HEIGHT = 800;
+
+export const Container = styled.div`
+  border: 1px solid black;
+  height: ${HEIGHT}px;
+  margin: 0 auto;
+  width:  ${WIDTH}px;
+`;
