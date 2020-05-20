74db7b0994ec056d079ae4ee1cd6256cd815542f
Hadrian Hughes <hadrian.hughes@and.digital>
2020-05-20 12:55:07 +0100

    Generate atoms on click

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -1,23 +1,43 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import { atom } from 'recoil';
 import { Container } from './styles';
 import { get } from '../../utils';
+import { Atom } from '../../types';
 
 const Graph = () => {
-  const elRef   = useRef(null);
+  const idCounter               = useRef(0);
+  const elRef                   = useRef(null);
+  const [vertices, setVertices] = useState(([] as Array<Atom>))
 
   const addVertex = e => {
     const elLeft  = get(['current', 'offsetLeft'])(elRef, 0);
     const elTop   = get(['current', 'offsetTop'])(elRef, 0);
     const clickX  = e.clientX - elLeft;
     const clickY  = e.clientY - elTop;
+    idCounter.current += 1;
 
-    console.log(`Make vertex at: ${clickX}, ${clickY}`)
+    setVertices([
+      ...vertices,
+      atom({
+        key: idCounter.current,
+        default: {
+          x: clickX,
+          y: clickY
+        }
+      })
+    ])
   };
 
   return (
     <Container
       onClick={addVertex}
-      ref={elRef}></Container>
+      ref={elRef}>
+      {
+        vertices.map(a => (
+          <div>{a.key}</div>
+        ))
+      }
+    </Container>
   );
 };
 
