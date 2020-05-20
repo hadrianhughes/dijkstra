e061e167e5f243b312471a863c603d6eedabb82b
Hadrian Hughes <hadrian.hughes@and.digital>
2020-05-20 17:04:43 +0100

    Create atoms in Graph component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import { atom } from 'recoil';
-import { Atom, EdgeT } from './types';
 import './App.css';
 import Graph from './components/Graph';
 import { RecoilRoot } from 'recoil';
 
-
-const makeVertex = (key: string, x: number, y: number): Atom => atom({
-  key,
-  default: { x, y }
-});
-
-const makeEdge = (vs: object) => (from: string, to: string): EdgeT => ({ from: vs[from], to: vs[to] });
-
 const vertices = {
-  A: makeVertex('vertex0', 150, 75),
-  B: makeVertex('vertex1', 340, 100),
-  C: makeVertex('vertex2', 530, 270),
-  D: makeVertex('vertex3', 710, 660)
+  A: { x: 150, y: 750 },
+  B: { x: 340, y: 100 },
+  C: { x: 530, y: 270 },
+  D: { x: 710, y: 660 },
 };
 
 const edges = [
@@ -32,7 +22,7 @@ const App = () => (
     <RecoilRoot>
       <Graph
         vertices={vertices}
-        edges={edges.map(e => makeEdge(vertices)(e.from, e.to))} />
+        edges={edges} />
     </RecoilRoot>
   </div>
 );
--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -1,12 +1,12 @@
 import React, { useRef } from 'react';
-import { EdgeT } from '../../types';
+import { atom } from 'recoil';
 import { Container } from './styles';
 import Vertices from './Vertices';
 import Edges from './Edges';
 
 interface PropTypes {
   vertices: object;
-  edges:    Array<EdgeT>;
+  edges:    Array<{ from: string, to: string }>;
 }
 
 export const GraphContext = React.createContext({
@@ -19,13 +19,25 @@ const Graph = ({ vertices, edges }: PropTypes) => {
   const getLeft = () => elRef.current.offsetLeft;
   const getTop  = () => elRef.current.offsetTop;
 
+  const vertexStates =
+    Object.keys(vertices)
+      .reduce((acc, k) => ({
+        ...acc,
+        [k]: atom({ key: `vertex_${k}`, default: vertices[k] })
+      }), {});
+
+  const realEdges = edges.map(e => ({
+    from: vertexStates[e.from],
+    to:   vertexStates[e.to]
+  }));
+
   return (
     <Container ref={elRef}>
       <GraphContext.Provider value={{
         getLeft, getTop
       }}>
-        <Edges items={edges} container={elRef} />
-        <Vertices items={vertices} />
+        <Edges items={realEdges} container={elRef} />
+        <Vertices items={vertexStates} />
       </GraphContext.Provider>
     </Container>
   );
