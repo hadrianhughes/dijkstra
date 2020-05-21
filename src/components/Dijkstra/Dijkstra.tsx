e79d55bc0419a6e6fe94c9b93dd1bf205593d1ff
Hadrian Hughes <hadrian.hughes@and.digital>
2020-05-21 11:17:34 +0100

    Replicate state management in the Dijkstra context
    - Will be subsequently making the Graph component purely presentational.

--- src/components/Dijkstra/Dijkstra.tsx
+++ src/components/Dijkstra/Dijkstra.tsx
@@ -1,14 +1,50 @@
 import React from 'react';
+import { atom } from 'recoil';
+import { Atom } from '../../types';
 
-export const DijkstraContext = React.createContext(null);
-
-interface PropTypes {
-  children: React.ReactNode;
+interface DijkstraProperties {
+  fromAtom?: Atom;
+  toAtom?:   Atom;
 }
 
-const DijkstraProvider = ({ children }: PropTypes) => {
+export const DijkstraContext = React.createContext({});
+
+interface PropTypes {
+  children:       React.ReactNode;
+  verticesConfig: object;
+  edgesConfig:    Array<{ from: string, to: string }>;
+}
+
+const DijkstraProvider = ({ children, verticesConfig, edgesConfig }: PropTypes) => {
+  const fromAtom = atom({
+    key:     'vertex_from',
+    default: Object.keys(verticesConfig)[0]
+  });
+
+  const toAtom = atom({
+    key:     'vertex_to',
+    default: Object.keys(verticesConfig)[1]
+  });
+
+  const vertices =
+    Object.keys(verticesConfig)
+      .reduce((acc, k) => ({
+        ...acc,
+        [k]: atom({ key: `vertex_${k}`, default: verticesConfig[k] })
+      }), {});
+
+  const edges = edgesConfig.map(e => ({
+    from: vertices[e.from],
+    to:   vertices[e.to]
+  }));
+
   return (
-    <DijkstraContext.Provider value={null}>
+    <DijkstraContext.Provider value={{
+      fromAtom,
+      toAtom,
+      vertices,
+      edges
+    }}>
       {children}
     </DijkstraContext.Provider>
   );
